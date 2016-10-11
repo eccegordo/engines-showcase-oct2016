7b5fe30183619e67f0922fe38f02119b3d006da1
Gordon Potter <eccegordo@gmail.com>
2016-10-11 15:02:14 -0700

    improve token form

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     githubAuth: Ember.inject.service('github-auth'),
-    tokenValue: 'abc123',
+    tokenValue: null,
     actions: {
       updateToken: function(token) {
         let githubAuth = this.get('githubAuth');
